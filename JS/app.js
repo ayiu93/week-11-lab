5ef364c667074b3611ea2c0c4fe37e79508c6f21
ayiu93 <anthonyyiu93@gmail.com>
2021-03-26 06:14:08 -0700

    created arrays and a for loop to check completion

--- JS/app.js
+++ JS/app.js
@@ -1,2 +1,16 @@
-// your JavaScript goes here, you can delete this comment
+
+const todoList = [
+    {todo: "clone repo for starter code", status: "complete"},
+    {todo: "link JS and CSS to index", status: "complete"},
+    {todo: "create for loop to check status & add to completed", status: "incomplete"},
+    {todo: "create for loop to log completed items", status: "incomplete"}        
+];
+
+const completedTodos = [];
+
+for(i=0;i<todoList.length;i++) {
+	if(todoList[i].status == "complete") {
+  	    completedTodos.push(todoList[i]);
+	}
+}
 
