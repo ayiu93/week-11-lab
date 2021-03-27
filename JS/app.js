ebee0bdbb7e4b8f968294678915d34b1390ab5cf
ayiu93 <anthonyyiu93@gmail.com>
2021-03-26 22:00:13 -0700

    added second for loop to log completed todos

--- JS/app.js
+++ JS/app.js
@@ -1,16 +1,32 @@
-
 const todoList = [
     {todo: "clone repo for starter code", status: "complete"},
     {todo: "link JS and CSS to index", status: "complete"},
-    {todo: "create for loop to check status & add to completed", status: "incomplete"},
+    {todo: "create for loop to check status & add to completed", status: "complete"},
     {todo: "create for loop to log completed items", status: "incomplete"}        
 ];
 
 const completedTodos = [];
 
+/** 
 for(i=0;i<todoList.length;i++) {
 	if(todoList[i].status == "complete") {
   	    completedTodos.push(todoList[i]);
 	}
 }
+**/
 
+for(const item of todoList) {
+	if(item.status == "complete") {
+  	completedTodos.push(item);
+  }
+}
+
+/**
+for(i=0;i<completedTodos.length;i++) {
+	console.log(completedTodos[i].todo);
+}
+**/
+
+for(const {todo = item} of completedTodos) {
+    console.log(todo);
+}
